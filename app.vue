07c7d5c6c98267a1ccb42e47d763e61ee1f8c9ea
Akash Mahadik <akashpm1996@gmail.com>
2024-04-01 01:59:58 +0530

    Add lenis for smooth scrolling

--- app.vue
+++ app.vue
@@ -1,3 +1,16 @@
+<script setup>
+import Lenis from '@studio-freight/lenis'
+
+onMounted(() => {
+  const lenis = new Lenis()
+
+  function raf(time) {
+    lenis.raf(time)
+    requestAnimationFrame(raf)
+  }
+  requestAnimationFrame(raf)
+})
+</script>
 <template>
   <div>
     <NuxtWelcome />
